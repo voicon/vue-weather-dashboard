0f9072de79ef2592478d87ed19bc997bd6a94ad2
vinh tran <xuanvinh@gmail.com>
2019-10-26 17:20:33 +0700

    first commit

---
+++ src/components/Content.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="pagecontent">
+    <p>This child components of Content.vue are:</p>
+    <ul>
+      <li v-for="child in childComponents">{{ child }}</li>
+    </ul>
+    {{ weather_data }}
+    <temp-var-chart :tempVar="tempVar"></temp-var-chart>
+    <today-highlights :highlights="highlights"></today-highlights>
+  </div>
+</template>
+
+<script>
+import TempVarChart from './TempVarChart.vue'
+import Highlights from './Highlights.vue'
+
+export default {
+    props: ["weather_data"],
+    components: {
+        'temp-var-chart': TempVarChart,
+        'today-highlights': Highlights
+    },
+    data () {
+        return {
+            childComponents: ['TempVarChart.vue', 'Highlights.vue'],
+            tempVar: this.weather_data.temperature,
+            highlights: this.weather_data.highlights,
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+}
+</script>
+
+<style>
+#pagecontent {
+  border: 1px solid black;
+  padding: 2px;
+}
+</style>
---
+++ src/components/Highlights.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="highlights">
+    <p>Weather Highlights:</p>
+    {{ highlights }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ["highlights"],
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+}
+</script>
+
+<style>
+
+</style>
